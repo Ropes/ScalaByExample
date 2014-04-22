7e6aef8b810507748de35c151b77827c876db94e
Josh Roppo <joshroppo@gmail.com>
2014-04-22 11:23:53 -0700

    Recursive Quicksort example added

--- src/main/scala/iterites.scala
+++ src/main/scala/iterites.scala
@@ -4,15 +4,28 @@ import scala._
 
 object Iterites extends App{
 
-    def filter_wat(wat: Array[Int]): Array[Int]= {
+    def filter_predicate(wat: Array[Int]): Array[Int]= {
         return wat.filter(p => p > 5)
+    }
+
+    def quicksort(arr: Array[Int]): Array[Int] = {
+        if(arr.length <= 1) arr
+        else{
+            val pivot = arr(arr.length /2)     
+            Array.concat(
+                quicksort(arr filter (p => p < pivot)), 
+                arr filter (p => pivot == p),
+                quicksort(arr filter (p => p > pivot)) )
+        }
     }
     
     override def main(args: Array[String]){
         val wat: Array[Int] = Array(1, 4, 6, 3, 2, 10, 100)
+        val hat = Array(5, 4, 1, 8, 90, 12)
         println(s"Length: ${wat.length}")
         println(s"Array.. ${wat.mkString("\n")}\n")
-        val out = filter_wat(wat)
+        val out = quicksort(hat)
         println(out.mkString(" "))
     }
+
 }
