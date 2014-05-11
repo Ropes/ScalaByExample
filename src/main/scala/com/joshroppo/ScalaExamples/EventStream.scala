24111cb1c3e1cf65b6198bcd2229946d12064b04
Josh Roppo <joshroppo@gmail.com>
2014-05-10 23:02:24 -0700

    Initial code for Pub/Sub event streaming in akka

---
+++ src/main/scala/com/joshroppo/ScalaExamples/EventStream.scala
@@ -0,0 +1,23 @@
+package com.joshroppo.learnakka
+
+import akka.actor.{Actor, Props, ActorSystem}
+
+class Subscriber(func: (String, Any) => Unit) extends Actor {
+    override def receive = { case (topic: String, payload: Any) => func(topic, payload) }
+}
+
+
+object EventStream{
+    val system = ActorSystem("actorsys")
+
+    def subscribe(func: (String, Any) => Option[Unit], name: String)={
+        val props = Props(classOf[Subscriber], func)
+        val subscriber = system.actorOf(props, name = name)
+        system.eventStream.subscribe(subscriber, classOf[(String, Any)])
+    }
+
+    def publish(topic: String, payload: Any){
+        system.eventStream.publish(topic, payload)
+    }
+}
+
