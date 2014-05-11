2ec15ced3bb96b920cd6a45dd0c03ba99a2ede20
Josh Roppo <joshroppo@gmail.com>
2014-05-10 23:52:57 -0700

    Coded up Auction Actor example, has compile errors

--- src/main/scala/com/joshroppo/ScalaExamples/Auction.scala
+++ src/main/scala/com/joshroppo/ScalaExamples/Auction.scala
@@ -15,3 +15,41 @@ case class AuctionConcluded(seller: Actor, client: Actor)
                                             extends AuctionReply
 case object AuctionFailed                   extends AuctionReply
 case object AuctionOver                     extends AuctionReply
+
+class Auction(seller: Actor, minBid: Int, closing: Date) extends Actor{
+    val timeToShutdown = 36000000
+    val bidIncrement = 10
+
+    def act(){
+        var maxBid = minBid - bidIncrement
+        var maxBidder: Actor = null
+        var running = true
+        while(running){
+            receiveWithin((closing.getTime() - new Date().getTime()){
+                case Offer(bid, client) =>
+                    if(bid >= maxBid + bidIncrement){
+                        if(maxBid >= minBid) maxBidder ! BeatenOffer(bid)
+                        maxBid = bid
+                        maxBidder = client
+                        client ! BestOffer
+                    } else {
+                        client ! BeatenOffer(maxBid)
+                    }
+                case Inquire(client) =>
+                    client ! Status(maxBid, closing)
+                case TIMEOUT =>
+                    if(maxBid >= minBid){
+                        val reply = AuctionConcluded(seller, maxBider)
+                        maxBidder ! reply
+                        seller ! reply
+                    } else {
+                        seller ! AuctionFailed
+                    }
+                    receiveWithin(timeToShutdown){
+                        case Offer(_, client) => client ! AuctionOver
+                        case TIMEOUT => running = false
+                    }
+            }
+        }
+    }
+}
