901b96c303a8f7c79a5f9fe9ab87efad0db81a93
Josh Roppo <joshroppo@gmail.com>
2014-05-10 23:02:50 -0700

    Initial setup for Auction from SBE

---
+++ src/main/scala/com/joshroppo/ScalaExamples/Auction.scala
@@ -0,0 +1,17 @@
+package com.joshroppo.auction
+
+import scala._
+import akka.actor._
+
+abstract class AuctionMessage
+case class Offer(bid: Int, client:Actor)    extends AuctionMessage
+case class Inquire(client: Actor)           extends AuctionMessage
+
+abstract class AuctionReply
+case class Status(asked: Int, expire: String) extends AuctionReply
+case object BestOffer                 extends AuctionReply
+case class BeatenOffer(maxBid: Int)   extends AuctionReply
+case class AuctionConcluded(seller: Actor, client: Actor)
+                                            extends AuctionReply
+case object AuctionFailed                   extends AuctionReply
+case object AuctionOver                     extends AuctionReply
